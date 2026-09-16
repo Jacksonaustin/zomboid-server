431be9c6088afffcfa76f60125a0cf2a4bd8c606
Jackson <jackson@Jacksons-MacBook-Air-2.local>
2026-09-16 00:15:22 -0500

    Pass -adminpassword so first run doesn't block on stdin

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ export HOME=/data/home
 
 : "${SERVER_NAME:?SERVER_NAME must be set}"
 : "${RCON_PASSWORD:?RCON_PASSWORD must be set}"
+: "${ADMIN_PASSWORD:?ADMIN_PASSWORD must be set}"
 
 mkdir -p "${HOME}/Zomboid/Server"
 
@@ -17,4 +18,9 @@ export SERVER_NAME RCON_PASSWORD
 envsubst < /home/steam/templates/servertest.ini.template \
     > "${HOME}/Zomboid/Server/${SERVER_NAME}.ini"
 
-exec /data/pzserver/start-server.sh -servername "${SERVER_NAME}"
+# -adminpassword is required for a non-interactive first run. Without it the
+# server prompts for an admin password on stdin, and with no TTY attached that
+# read throws NoSuchElementException and the process dies immediately.
+exec /data/pzserver/start-server.sh \
+    -servername "${SERVER_NAME}" \
+    -adminpassword "${ADMIN_PASSWORD}"
